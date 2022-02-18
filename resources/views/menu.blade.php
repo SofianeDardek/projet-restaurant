c232646db5f875555d7f56a59c90524b3598771d
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-18 19:24:45 +0100

    Création d'une nouvelle vue
    - Ajout de code

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,82 @@
+@extends('templates.app')
+
+@section('content')
+<div class="menu-container">
+            <h1>Petit déjeuner</h1>
+            <hr class="divider"></hr>
+            <div class="box">
+                <div class="image">
+                    <img src="images/img1.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Viande Fraîche</span>
+                    <span class="cost">5,00€</span>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img2.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Salade</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img3.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Poulet rôtis</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img1.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Poulet rôtis</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+        </div>
+
+        <div class="menu-container">
+            <h1>Déjeuner</h1>
+            <hr class="divider"></hr>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img2.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Salade</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img3.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Poulet rôtis</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+
+            <div class="box">
+                <div class="image">
+                    <img src="images/img1.jpg" alt="">
+                </div>
+                <div class="text">
+                    <span class="item-name">Poulet rôtis</span>
+                    <span class="cost">3,40€</span>
+                </div>
+            </div>
+        </div>
+@endsection('content')
