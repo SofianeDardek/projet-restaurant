335970ac18f42955b66fbd18eb6b4f42622b8acb
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-25 17:17:08 +0100

    Ajout de code
    - Modification de la route menu

--- app/Http/Controllers/MenuController.php
+++ app/Http/Controllers/MenuController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Categorie;
 
 class MenuController extends Controller
 {
     public function index(){
-        return view('menu');
+
+        $categorie1 = Categorie::find(1);
+        $categorie2 = Categorie::find(2);
+
+        return view('menu', [
+            'categorie1' => $categorie1,
+            'categorie2' => $categorie2
+        ]);
     }
 }
