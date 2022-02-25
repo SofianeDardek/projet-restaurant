6976031725b56a509f1e024a717f847c900df183
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-25 09:21:29 +0100

    Ajout de code
    - Ajout de la route réservation
    - nommage des routes

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
+use App\Http\Controllers\MenuController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,4 +15,5 @@ use App\Http\Controllers\HomeController;
 |
 */
 
-Route::get('/', [HomeController::class, 'index']);
+Route::get('/', [HomeController::class, 'index'])->name('home');
+Route::get('/reservation', [MenuController::class, 'index'])->name('menu');
