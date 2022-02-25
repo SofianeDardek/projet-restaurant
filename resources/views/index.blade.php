7c8a9be9e0d62b4422dd3822f0092e3397c6319c
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-25 09:19:14 +0100

    Ajout de code
    - Ajout d'images
    - Ajout de texte

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,42 @@
 @extends('templates/app')
 
 @section('content')
-    <h1>Hello World</h1>
+        <div class="home-grid">
+                <div class="home-banner">
+                    <img src="images/home.jpg">
+                </div>
+
+                @foreach($news as $new)
+                <div class="news">
+                    <div class="image-news">
+                        <img src="{{ $new->image->path }}">
+                    </div>
+                    <div class="desc">
+                        <h1>{{ $new->title }}</h1>
+                        <span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
+                    </div>
+                </div>
+
+                <!-- <div class="news">
+                    <div class="image-news">
+                        <img src="images/actu2.jpg">
+                    </div>
+                    <div class="desc">
+                        <h1>Actu 2</h1>
+                        <span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
+                    </div>
+                </div>
+
+                <div class="news">
+                    <div class="image-news">
+                        <img src="images/actu1.jpg">
+                    </div>
+                    <div class="desc">
+                        <h1>Actu 1</h1>
+                        <span>Lorem ipsum dolor sit amet. Lorem ipsum dolor sit, amet consectetur adipisicing elit.</span>
+                    </div>
+                </div> -->
+                @endforeach
+
+        </div>
 @endsection('content')
