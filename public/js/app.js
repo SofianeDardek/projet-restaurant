10fc171c6bdeaf7f5d35de253a8af81d29c145d8
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-26 21:24:29 +0100

    Ajout de code
    - Ajout d'un script Leaflet pour OpenStreetMap

--- public/js/app.js
+++ public/js/app.js
@@ -2062,6 +2062,17 @@ module.exports = {
 
 __webpack_require__(/*! ./bootstrap */ "./resources/js/bootstrap.js");
 
+var map = L.map('map').setView([3.854213494326459, 11.50114188187485], 15);
+L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+  minZoom: 10,
+  maxZoom: 20,
+  id: 'mapbox/streets-v11',
+  accessToken: 'your.mapbox.access.token'
+}).addTo(map);
+var marker = L.marker([3.854213494326459, 11.50114188187485]).addTo(map);
+marker.bindPopup('<b>Oburo</b>');
+
 /***/ }),
 
 /***/ "./resources/js/bootstrap.js":
