c83312a509da038a045c50767cbfe04ee65706cd
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-02-26 21:31:58 +0100

    Création de la migration items
    - Initialisation de la table

---
+++ database/migrations/2022_02_25_083518_create_items_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateItemsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('items', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedSmallInteger('categorie_id');
+            $table->string('title');
+            $table->string('path');
+            $table->unsignedSmallInteger('price');
+            $table->timestamps();
+
+            $table->foreign('categorie_id')->references('id')->on('categories');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('items');
+    }
+}
