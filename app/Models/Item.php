06612221a4e36f74df3a8f506006068daffd0d57
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-03 19:07:22 +0100

    Création du model Item

---
+++ app/Models/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use App\Models\Categorie;
+
+class Item extends Model
+{
+    use HasFactory;
+
+    public function categorie(){
+        return $this->belongsTo(Categorie::class);
+    }
+}
