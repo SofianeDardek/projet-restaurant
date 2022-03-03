056c765561f427c9c9308b424127da6f09ab9fc4
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-03 19:01:43 +0100

    Modification du code
    - Modification du formulaire

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,5 +1,29 @@
 @extends('templates/app')
 
 @section('content')
-<h1>test</h1>
+<div class="contact-grid">
+
+    <div id="map">
+    
+    </div>
+
+    
+    <div class="contact-box">
+        <div class="contact-container">
+            <h1>Contactez Nous</h1>
+            <form method="POST">
+                @csrf
+                <input type="text" name="first_name" placeholder="Nom" class="name">
+                <input type="text" name="last_name" placeholder="Prénom" class="first-name">
+                <input type="text" name="email" placeholder="Email" class="email">
+                <input type="text" name="tel" placeholder="téléphone" class="tel">
+                <textarea rows="6" placeholder="Message" name="message"></textarea>
+                <button>Envoyer</button>
+
+            </form>
+        </div>
+    </div>
+    
+        
+</div>
 @endsection('content')
