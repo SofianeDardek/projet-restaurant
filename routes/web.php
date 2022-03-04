05600c06c3d3f7135c574119d0070e56afb40b31
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-04 08:22:17 +0100

    Ajout de code
    - Ajout du namespace ReservationController

--- routes/web.php
+++ routes/web.php
@@ -4,6 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\MenuController;
 use App\Http\Controllers\ContactController;
+use App\Http\Controllers\ReservationController;
 
 /*
 |--------------------------------------------------------------------------
