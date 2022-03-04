1b5400717cdcd1ec5e6027010498390fb0cd389f
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-04 08:50:01 +0100

    Supression de code
    - Supression du script javascript pour la carte openstreetmap

--- resources/views/templates/app.blade.php
+++ resources/views/templates/app.blade.php
@@ -38,20 +38,5 @@
             </div>
         </div>
     </footer>
-    <script src="{{asset('js/app.js')}}"></script>
-    <script>
-        let map = L.map('map').setView([3.854213494326459, 11.50114188187485], 15);
-
-    L.tileLayer('https://{s}.tile.openstreetmap.fr/osmfr/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
-        minZoom: 10,
-        maxZoom: 20,
-        id: 'mapbox/streets-v11',
-        accessToken: 'your.mapbox.access.token'
-    }).addTo(map);
-
-    let marker = L.marker([3.854213494326459, 11.50114188187485]).addTo(map);
-    marker.bindPopup('<b>Oburo</b>');
-    </script>
 </body>
 </html>
