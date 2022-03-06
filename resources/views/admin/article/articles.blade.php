62574f462bfc118789eb646c529783fb0a0cb14e
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-06 16:24:29 +0100

    Ajout de code
    - Ajout d'un tableau regroupant la liste des articles
    - Ajout de bouttons: 'Ajouter', 'Modifier', 'Supprimer'

--- resources/views/admin/article/articles.blade.php
+++ resources/views/admin/article/articles.blade.php
@@ -1,4 +1,32 @@
+@extends('templates/admin')
 
-@foreach($news as $new)
-<h1>{{ $new->title }}</h1>
-@endforeach
+@section('content')
+<div class="container">
+    <div class="pt-3">
+        <a class="btn btn-success" href="{{ route('article.create') }}">Ajouter un article</a>
+    </div>
+    <table class="table">
+    <thead>
+        <tr>
+        <th scope="col">#</th>
+        <th scope="col">Actualité</th>
+        <th scope="col">Date</th>
+        <th scope="col">Action</th>
+        </tr>
+    </thead>
+    <tbody>
+    @foreach($news as $new)
+        <tr>
+            <th scope="row">{{ $new->id }}</th>
+            <td>{{ $new->title }}</td>
+            <td>Otto</td>
+            <td>
+                <a class="btn btn-primary">Modifier</a>
+                <a class="btn btn-danger">Supprimer</a>
+            </td>
+        </tr>
+        @endforeach
+    </tbody>
+    </table>
+</div>
+@endsection('content')
