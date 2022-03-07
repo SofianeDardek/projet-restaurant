58a6e44814b70a7230fa0f31470ce973ebe56ba0
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-07 16:22:23 +0100

    Ajout de code
    - Ajout de requetes dans la function create

--- app/Http/Controllers/Admin/ArticleController.php
+++ app/Http/Controllers/Admin/ArticleController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
 use App\Models\News;
+use App\Models\Image;
 use Illuminate\Http\Request;
 
 class ArticleController extends Controller
@@ -19,7 +20,7 @@ class ArticleController extends Controller
 
     public function create()
     {
-
+        return view('admin.article.create');
     }
 
     public function update()
@@ -30,6 +31,31 @@ class ArticleController extends Controller
     public function delete()
     {
 
+    }
+
+    public function store(Request $request)
+    {
+        $validate = $request->validate([
+            'title' => ['required'],
+            'description' => ['required'],
+            'image' => ['required']
+        ]);
+
+        $articles = new News();
+
+        $articles->title = $request->title;
+        $articles->description = $request->description;
+        // $articles->path = $request->image;
+
+        $image = new Image();
+        $image->path = $request->image;
+        $image->new_id = 6;
+        dd($articles->id);
+
+        $image->save();
+
+        $articles->save();
+        
     }
 
 }
