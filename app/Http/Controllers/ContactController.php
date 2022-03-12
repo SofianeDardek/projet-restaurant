613ac1c3101715a0da8be59f195a54b938a86bc7
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-12 19:13:52 +0100

    Ajout de code
    - Ajout de la fonction store qui stocke les infos postées dans
    le formulaire

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -3,10 +3,34 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Contact;
 
 class ContactController extends Controller
 {
-    public function index(){
+    public function index()
+    {
         return view('contact');
     }
+
+    public function store(Request $request)
+    {
+    $validate = $request->validate([
+            'first_name' => ['required'],
+            'last_name' => ['required'],
+            'tel' => ['required'],
+            'email' => ['required'],
+            'message' => ['required']
+        ]);
+
+        $contact = new Contact;
+        $contact->first_name = $request->first_name;
+        $contact->last_name = $request->last_name;
+        $contact->email = $request->email;
+        $contact->phone_number = $request->tel;
+        $contact->message = $request->message;
+        $contact->save();
+
+        // dd($request->message);
+    }
+    
 }
