085f5a2d64e0974c2293d37e58083612bc161d01
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-13 17:34:07 +0100

    Ajout de code
    Ajout d'une pop up de confirmation quand on clique sur le boutton
    supprimer

--- resources/views/admin/article/articles.blade.php
+++ resources/views/admin/article/articles.blade.php
@@ -3,6 +3,9 @@
 @section('content')
 <div class="container">
     <div class="pt-3">
+    @if(Session()->has('success'))
+            <div class="alert alert-success">{{ Session()->get('success') }}</div>
+        @endif
         <a class="btn btn-success" href="{{ route('article.create') }}">Ajouter un article</a>
     </div>
     <table class="table">
@@ -22,11 +25,36 @@
             <td>Otto</td>
             <td>
                 <a class="btn btn-primary">Modifier</a>
-                <a class="btn btn-danger">Supprimer</a>
+                <a class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#confirmDelete">Supprimer</a>
             </td>
         </tr>
+
+              <!-- Modal -->
+      <div class="modal fade" id="confirmDelete" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="exampleModalLabel">Message important</h5>
+              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+              Êtes vous sûr de vouloir supprimer cette actualité?
+            </div>
+            <div class="modal-footer">
+              <form method="POST" action="{{ route('article.delete', ['article' => $new->id]) }}">
+                @csrf
+              <input type="hidden" name="_method" value="delete">
+              <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Fermer</button>
+              <button type="submit" class="btn btn-warning">Confirmer</a>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
         @endforeach
     </tbody>
     </table>
+
 </div>
 @endsection('content')
