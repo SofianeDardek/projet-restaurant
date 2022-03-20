fa2588c83b0c5fd266e47566ef05dcbb21167818
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-20 15:31:24 +0100

    Ajout de code
    - Ajouts des infos de la base de donnée de la table "site_infos"

--- resources/views/admin/home.blade.php
+++ resources/views/admin/home.blade.php
@@ -7,18 +7,32 @@
 
     <div class="row">
         <div class="col-5">
-            <form>
+            <form method="POST">
+                @csrf
+                @if($errors->any())
+                    <div class="alert alert-danger">
+                    @foreach($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                    @endforeach
+                    </div>
+                @endif
+    
+                @if(Session()->has('success'))
+                    <div class="alert alert-success">
+                        <p>{{ Session()->get('success') }} </p>
+                    </div>
+                @endif
                 <div class="mb-3">
                     <label for="exampleInputEmail1" class="form-label">Adresse email</label>
-                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+                    <input name="email" type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" value="{{ $info->email }}">
                 </div>
                 <div class="mb-3">
                     <label for="exampleInputPassword1" class="form-label">Adresse</label>
-                    <input type="password" class="form-control" id="exampleInputPassword1">
+                    <input name="adress" type="text" class="form-control" id="exampleInputPassword1" value="{{ $info->adress }}">
                 </div>
                 <div class="mb-3">
                     <label for="phone" class="form-label">Télephone</label>
-                    <input type="password" class="form-control" id="phone">
+                    <input name="phone" type="text" class="form-control" id="phone" value="{{ $info->phone_number }}">
                 </div>
                 <button type="submit" class="btn btn-primary">Submit</button>
             </form>
