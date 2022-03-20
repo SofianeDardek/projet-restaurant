cd4db8e8a2113ac47853228f1078e8bce7249f0a
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-20 15:12:40 +0100

    Ajout de code
    - Ajout de la fonction "index" qui retourne la vue
    - Ajout de la fonction "store" qui stocke les données postées

--- app/Http/Controllers/ReservationController.php
+++ app/Http/Controllers/ReservationController.php
@@ -2,12 +2,44 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Day;
+use App\Models\Reservation;
+use DateTime;
 use Illuminate\Http\Request;
+
+
 
 class ReservationController extends Controller
 {
-    public function index()
+    public function index(Day $day)
     {
-        return view('reservation');
+        $days = $day->all();
+
+        return view('reservation', [
+            'days' => $days
+        ]);
+    }
+
+    public function store(Request $request, Reservation $reservation)
+    {
+        $today = new DateTime;
+        $validate = $request->validate([
+            'last_name' => ['required'],
+            'phone' => ['required'],
+            'date' => ['required', 'date', 'after_or_equal:'.$today->format('d/m/Y')],
+            'hour' => ['required'],
+            'person' => ['required', 'integer', 'between:0,3'],
+        ]);
+
+      
+        $reservation->last_name = $request->last_name;
+        $reservation->phone = $request->phone;
+        $reservation->date = $request->date;
+        $reservation->hours = $request->hour;
+        $reservation->person = $request->person;
+
+        $reservation->save();
+        
+        return back()->with('success', 'Votre reservation a été enregistré avec succès');
     }
 }
