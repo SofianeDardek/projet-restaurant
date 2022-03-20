dd955ec4b710c09d7a662f1c746c912ee77ae35b
sofiane dardek <sofiane.dardek@hotmail.fr>
2022-03-20 13:19:52 +0100

    Ajout de code
    - Ajout d'une boucle qui affiche les jours

--- resources/views/reservation.blade.php
+++ resources/views/reservation.blade.php
@@ -1,38 +1,73 @@
 @extends('templates/app')
 
 @section('content')
-<div class="contact-box">
-        <div class="contact-container">
-            <h1>Reservationd</h1>
-            <form method="POST" action="#">
-                @csrf
-                <div class="input-block">
-                <input type="text" name="first_name" placeholder="Nom" class="name">
-                <!-- @if($errors->has('first_name'))
-                <label class="error" for="fist_name">Veuillez remplir ce champ</label>
-                @endif -->
-                </div>
+<div class="grid-p-reservation">
+    <div class="reservation-box">
+            <div class="reservation-container">
+                <h1>Reservation</h1>
+                <form method="POST" action="#">
+                    @csrf
+                    <div class="input-block">
+                    <input type="text" name="last_name" placeholder="Nom" class="name">
+                    @if($errors->has('last_name'))
+                    <label class="error" for="last_name">Veuillez remplir ce champ</label>
+                    @endif
+                    </div>
 
-                <div class="input-block">
-                <input type="text" name="last_name" placeholder="Prénom" class="first-name">
-                <!-- @if($errors->has('last_name'))
-                <label class="error" for="last_name">Veuillez remplir ce champ</label>
-                @endif -->
-                </div>
+                    <div class="input-block" id="couvert">
+                    <select name="person" class="couvert">
+                        <option value="1">1 couvert</option>
+                        <option value="2">2 couvert</option>
+                        <option value="3">3 couvert</option>
+                    </select>
+                    @if($errors->has('person'))
+                    <label class="error" for="person">Veuillez remplir ce champ</label>
+                    @endif
+                    </div>
 
-                <div class="input-block">
-                <input type="text" name="email" placeholder="Email" class="email">
-           
-    
-     
-                </div>
+                    
+                    <div class="input-block" id="date">
+                    <input type="date" min="2022-03-18" name="date" id="date" class=" form-control" value="2022-04-01">
+                    @if($errors->has('date'))
+                    <label class="error" for="date">Veuillez remplir ce champ</label>
+                    @endif
+                    </div>
+                    
+
+                    <div class="input-block" id="hour">
+                    <input type="time" name="hour" id="heure" class=" form-control" value="18:00:00">
+                    @if($errors->has('hour'))
+                    <label class="error" for="hour">Veuillez remplir ce champ</label>
+                    @endif
+                    </div>
 
 
-                <input type="text" name="tel" placeholder="téléphone" class="tel">
-                <textarea rows="6" placeholder="Message" name="message"></textarea>
-                <button class="cta-send">Envoyer</button>
+                    <div class="input-block" id="phone">
+                    <input type="text" name="phone" placeholder="Téléphone" class="tel">
+                    @if($errors->has('phone'))
+                    <label class="error" for="phone">Veuillez remplir ce champ</label>
+                    @endif
+                    </div>
+                    
+                    <button class="cta-send">Envoyer</button>
 
-            </form>
+                </form>
+            </div>
         </div>
-    </div>
+
+        <div class="hours-box">
+            <div class="hours-container">
+                <h1>Horraires</h1>
+
+                @foreach($days as $day)
+                <div class="days-container">
+                    <div class="days">
+                        <h2>{{ $day->day }}</h2>
+                        <p>Fermé</p>
+                    </div>
+                </div>
+                @endforeach
+            </div>
+        </div>
+</div>
 @endsection('content')
